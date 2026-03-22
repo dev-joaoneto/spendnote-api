f73a37ba8b8ec605b0dc55597b0623be6c9d0fc7
netinho537ia <netinhooo.537@gmail.com>
2026-03-22 13:05:32 -0300

    att cors

--- src/app.ts
+++ src/app.ts
@@ -19,6 +19,14 @@ app.register(cors, {
 	methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
 });
 
+app.options('*', async (request, reply) => {
+  reply
+    .header('Access-Control-Allow-Origin', request.headers.origin || '*')
+    .header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
+    .header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    .send();
+});
+
 app.register(routes, {prefix: "/api"});
 
 export default app;
