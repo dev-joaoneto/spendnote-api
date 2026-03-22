1ea25b373db73851e99be6a94726ceb2786aa659
netinho537ia <netinhooo.537@gmail.com>
2026-03-22 13:15:34 -0300

    teste cors

--- src/app.ts
+++ src/app.ts
@@ -11,10 +11,7 @@ const app: FastifyInstance = Fastify({
 });
 
 app.register(cors, {
-	origin: [
-    "http://localhost:5175",
-    "https://spendnote-frontend.xgg4n8.easypanel.host"
-  ],
+	origin: true,
   credentials: true,
 	methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
 });
